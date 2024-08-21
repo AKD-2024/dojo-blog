9c9d6b57ddf018d0471bfd5182aab272e4976b03
Anjali <anjali.dubey@comprotechnologies.com>
2024-08-21 17:56:15 +0530

    Vue: Training.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">toggle posts</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostList from "../components/PostList.vue";
+import { ref } from "vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      {
+        title: "Welcome to the blog",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit unde, quia fugit possimus velit eaque doloribus assumenda itaque commodi, ducimus, quis neque qui sit consequuntur quod. Ipsam, quis impedit.",
+        id: 1,
+      },
+      {
+        title: "Top 5 CSS tips",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit unde, quia fugit possimus velit eaque doloribus assumenda itaque commodi, ducimus, quis neque qui sit consequuntur quod. Ipsam, quis impedit. Numquam!",
+        id: 2,
+      },
+    ]);
+
+    const showPosts = ref(true);
+
+    return { posts, showPosts };
+  },
+};
 </script>
